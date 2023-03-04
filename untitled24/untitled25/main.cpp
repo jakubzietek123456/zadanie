c7e7194c6dc5cd94de30697149565446af1c8ac0
jakubzietek123456 <124282075+jakubzietek123456@users.noreply.github.com>
2023-03-04 18:40:33 +0100

    Add files via upload

---
+++ untitled24/untitled25/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+    c = a * b;
+    cout << "Wynik mnożenia a i b to: " << c << endl;
+    return 0;
+}
